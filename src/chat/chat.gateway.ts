6b9d5a18668a09a7c2b04a26c391e4d9a5575015
PedroPeyroteo <pedropeyroteo@outlook.com>
2024-07-20 13:21:50 +0100

    feat: added basic structure

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,55 @@
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: (origin, callback) => {
+      if (!origin || origin.startsWith('http://192.168.1.')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST'],
+  },
+})
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private readonly logger = new Logger(ChatGateway.name);
+
+  @WebSocketServer() io: Server;
+
+  afterInit() {
+    this.logger.log('Initialized');
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    const { sockets } = this.io.sockets;
+    this.logger.log(`Client id: ${client.id} connected`);
+    this.logger.debug(`Number of connected clients: ${sockets.size}`);
+  }
+
+  handleDisconnect(client: any) {
+    this.io.emit(`pong`, {
+      clientId: client.id,
+      msg: 'HAS DISCONNECTED',
+    });
+    this.logger.log(`Client id: ${client.id} disconnected`);
+  }
+
+  @SubscribeMessage('ping')
+  handleMessage(client: any, data: any) {
+    this.logger.log(`Message received from client id: ${client.id}`);
+    this.logger.debug(`Payload: ${data}`);
+    this.io.emit('ping', { clientId: client.id, msg: data });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Enable CORS for the entire application
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || origin.startsWith('http://192.168.1.')) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST'],
+    credentials: true, // If your application requires credentials
+  });
+
+  const port = 3000; // Ensure this port is correct
+  await app.listen(port, '0.0.0.0');
+  console.log(`Nest server is running on http://0.0.0.0:${port}`);
 }
 bootstrap();
